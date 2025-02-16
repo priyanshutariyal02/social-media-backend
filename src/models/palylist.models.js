effbc5f3b0ff69410defa184f343d1b271b03efb
priyanshutariyal02 <tariyalpriyanshu6@gmail.com>
2025-02-16 19:17:50 +0530

    feat: add comment, like, playlist, tweet models

---
+++ src/models/palylist.models.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const playlistSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    video: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const PlayList = mongoose.model("Playlist", playlistSchema);
